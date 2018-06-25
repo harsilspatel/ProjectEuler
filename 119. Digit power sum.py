ee2fabf6386525dc8aaf66fd151c325fecc44c7a
harsilspatel <harsilspatel@gmail.com>
2018-06-26 00:05:27 +1000

    119. (optimized)

--- 119. Digit power sum.py
+++ 119. Digit power sum.py
@@ -1,18 +1,51 @@
+powerEndings = [
+	[0],
+	[1],
+	[2,4,8,6],
+	[3,9,7,1],
+	[4,6],
+	[5],
+	[6],
+	[7,9,3,1],
+	[8,4,2,6],
+	[9,1],
+]
+
+def isPossible(base, number):
+	baseLastDigit = base % 10
+	numberLastDigit = number % 10
+	try:
+		return powerEndings[baseLastDigit].index(numberLastDigit)
+	except ValueError:
+		return -1
+
+def sumDigits(number):
+	theSum = 0
+	while number != 0:
+		theSum += number % 10
+		number //= 10
+	return theSum
+
 i = 10
 count = 0
 
 while count < 30:
 	i += 1
-	digitSum = sum(map(int, str(i)))
-	if digitSum == 1:
+	digitSum = sumDigits(i)
+	
+	startingPower = isPossible(digitSum, i)
+	
+	if startingPower == -1:
 		continue
-	power = digitSum
-	n = 1
-	while power < i:
-		n += 1
-		power *= digitSum
+	
+	power = digitSum**(startingPower+1)
+	multiple = digitSum**len(powerEndings[digitSum%10])
+	
+	while power <= i:
 		if power == i:
-			print('answer:',count+1, digitSum, n, i)
+			print('answer:',count+1, digitSum, i)
 			count += 1
 			break
+		power *= multiple
+		
 print(i)
